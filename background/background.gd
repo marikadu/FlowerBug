6c51ccbd913156da66986585ec497f80011d8042
ikigi_ki <marika.duhhanina@student.hamk.fi>
2025-03-25 10:50:35 +0200

    updated the sky and the pollen progress bar

--- background/background.gd
+++ background/background.gd
@@ -1,18 +1,33 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var clouds_morning: TextureRect = $Clouds_morning
+@onready var clouds_noon: TextureRect = $Clouds_noon
+@onready var clouds_evening: TextureRect = $Clouds_evening
+
+
 func _ready() -> void:
+	
+	clouds_morning.visible = false
+	clouds_noon.visible = false
+	
+	# wait a frame before loading
+	await get_tree().process_frame
+	
+	
+	
 	# different background depending on the current level
 	if Global.current_scene_name == 1:
 		$BgSky.frame = 0
+		clouds_morning.visible = true
 		
 	elif Global.current_scene_name == 2:
 		$BgSky.frame = 1
+		clouds_noon.visible = true
 		
 	elif Global.current_scene_name == 3:
 		$BgSky.frame = 2
-
+		clouds_evening.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
